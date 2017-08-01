566163335cc84ca0c550b6a9c140a79aa6685401
j0v0049 <j0v0049@lj0v0049win10.homeoffice.Wal-Mart.com>
2017-08-01 08:59:23 -0500

    Created class Message

---
+++ src/main/java/org/jaini/messenger/model/Message.java
@@ -0,0 +1,38 @@
+package org.jaini.messenger.model;
+
+import java.util.Date;
+
+public class Message {
+
+	private long id;
+	private String message;
+	private Date createDate;
+	private String author;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public Date getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	
+}
