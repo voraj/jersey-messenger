6884743061713618433e69370d7c318e038a6794
j0v0049 <j0v0049@lj0v0049win10.homeoffice.Wal-Mart.com>
2017-08-02 17:46:26 -0500

    Added CRUD operations to MessageService

--- src/main/java/org/jaini/messenger/services/MessageService.java
+++ src/main/java/org/jaini/messenger/services/MessageService.java
@@ -2,19 +2,40 @@ package org.jaini.messenger.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import org.jaini.messenger.database.DatabaseClass;
 import org.jaini.messenger.model.Message;
 
 public class MessageService {
+
+	private Map<Long, Message> messages = DatabaseClass.getAllMessages();
+
+	public List<Message> getAllMessages() {
+		return new ArrayList<Message>(messages.values());
+
+	}
 	
-	public List<Message> getAllMessages(){
-		Message m1=new Message(1L, "Hello world", "Jaini");
-		Message m2=new Message(2L, "Wohoo", "Jaini");
-		List<Message> list=new ArrayList<Message>();
-		list.add(m1);
-		list.add(m2);
-		return list;
-		
+	public Message getMessage(long id){
+		return messages.get(id);
+	}
+	
+	public Message addMessage(Message message){
+		message.setId(messages.size()+1);
+		messages.put(message.getId(), message);
+		return message;
+	}
+	
+	public Message updateMessage(Message message){
+		if(messages.containsKey(message.getId())){
+			messages.put(message.getId(), message);
+			return message;
+		}
+		return null;
+	}
+	
+	public Message removeMessage(long id){
+		return messages.remove(id);
 	}
 
 }
