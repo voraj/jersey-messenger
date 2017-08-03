afe70eafa9fb19adda0f879c614b5b13acd916a4
j0v0049 <j0v0049@lj0v0049win10.homeoffice.Wal-Mart.com>
2017-08-03 08:50:52 -0500

    Added Profile

---
+++ src/main/java/org/jaini/messenger/model/Profile.java
@@ -0,0 +1,64 @@
+package org.jaini.messenger.model;
+
+import java.util.Date;
+
+public class Profile {
+
+	private long id;
+	private String profileName;
+	private String firstName;
+	private String lastName;
+	private Date createDate;
+
+	public Profile() {
+	}
+
+	public Profile(long id, String profileName, String firstName, String lastName) {
+		this.id = id;
+		this.profileName = profileName;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.createDate = new Date();
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getProfileName() {
+		return profileName;
+	}
+
+	public void setProfileName(String profileName) {
+		this.profileName = profileName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+}
