2d03aa1f8c4d4e5e3b056a68ce49b1b093195b77
j0v0049 <j0v0049@lj0v0049win10.homeoffice.Wal-Mart.com>
2017-08-06 18:28:55 -0500

    Added Profile Service

--- src/main/java/org/jaini/messenger/database/DatabaseClass.java
+++ src/main/java/org/jaini/messenger/database/DatabaseClass.java
@@ -4,13 +4,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.jaini.messenger.model.Message;
+import org.jaini.messenger.model.Profile;
 
 public class DatabaseClass {
 	
 	private static HashMap<Long, Message> messages=new HashMap<Long, Message>();
+	private static HashMap<String, Profile> profiles=new HashMap<String, Profile>();
 	
 	public static Map<Long, Message> getAllMessages(){
 		return messages;
 	}
 	
+	public static Map<String, Profile> getProfiles(){
+		return profiles;
+	}
 }
---
+++ src/main/java/org/jaini/messenger/services/ProfileService.java
@@ -0,0 +1,39 @@
+package org.jaini.messenger.services;
+
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Map;
+
+import org.jaini.messenger.database.DatabaseClass;
+import org.jaini.messenger.model.Profile;
+
+public class ProfileService {
+
+	private Map<String, Profile> profiles = DatabaseClass.getProfiles();
+
+	public ArrayList<Profile> getAllProfiles() {
+		return new ArrayList<Profile>(profiles.values());
+	}
+
+	public Profile getProfile(String profileName) {
+		return profiles.get(profileName);
+	}
+
+	public Profile addProfile(Profile profile) {
+		profile.setId(profiles.size() + 1);
+		profiles.put(profile.getProfileName(), profile);
+		return profile;
+	}
+
+	public Profile updateProfile(Profile profile) {
+		if (profile.getId() <= 0)
+			return null;
+		profiles.put(profile.getProfileName(), profile);
+		return profile;
+	}
+
+	public Profile removeProfile(String profileName) {
+		return profiles.remove(profileName);
+	}
+
+}
